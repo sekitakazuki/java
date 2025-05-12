bca6470bf212ae7c7696c8f5df487aeef08e3168
kazuki <kazuki@localhost>
2025-05-12 15:44:06 +0900

    最初のコミット

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		String name = "たろう";
+		double d = 5.8;
+		int i = (int)d;
+		System.out.println(d);
+		System.out.println(4*6);
+	}
+}
